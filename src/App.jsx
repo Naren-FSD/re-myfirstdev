35693d495bdbb1b4d98c75447d721b8ba0f1cf56
Naren-FSD <connect4mail@gmail.com>
2025-06-23 07:57:23 +0530

    using javascript

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,8 @@ function App(){
     <>
     <Header />
     <Paragraph />
-    <Header />
-    <Paragraph />
-    <Header />
-    <Paragraph />
+    
+    
     
     
   </>
--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -1,6 +1,9 @@
 //paragraph component
 function Paragraph(){
-  return <p>thanks for watching!</p>;
+  let x = 12;
+  let y = 5;
+  let z = x+y;
+  return <p>The sum of x and y is: {z}</p>;
 }
 
 export default Paragraph;
